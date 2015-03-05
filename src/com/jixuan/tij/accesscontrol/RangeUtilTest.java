6a857dfec1b2bbdee0573ef74b9095d2671881b5
jixuan <420310979@qq.com>
2015-03-05 14:20:41 +0800

    生成整数数组小工具

---
+++ src/com/jixuan/tij/accesscontrol/RangeUtilTest.java
@@ -0,0 +1,19 @@
+package com.jixuan.tij.accesscontrol;
+
+import java.util.Arrays;
+
+import static com.jixuan.tij.util.Print.println;
+import static com.jixuan.tij.util.Range.range;
+
+/**
+ * @author jixuan
+ *         Create on 15/3/5.
+ */
+public class RangeUtilTest {
+    public static void main(String[] args) {
+        println(Arrays.toString(range(5)));
+        println(Arrays.toString(range(3,10)));
+        println(Arrays.toString(range(6,20,2)));
+        println(Arrays.toString(range(5,20,2)));
+    }
+}
--- src/com/jixuan/tij/util/Range.java
+++ src/com/jixuan/tij/util/Range.java
@@ -5,4 +5,29 @@ package com.jixuan.tij.util;
  *         Create on 15/3/5.
  */
 public class Range {
+    public static int[] range(int n) {
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = i;
+        }
+        return result;
+    }
+
+    public static int[] range(int start, int end) {
+        int length = end - start + 1;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = i + start;
+        }
+        return result;
+    }
+
+    public static int[] range(int start, int end, int step) {
+        int length = (end - start) / step + 1;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = start + i * step;
+        }
+        return result;
+    }
 }
