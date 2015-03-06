95cf731b0a351cebd5fd0a9a897b6ec581c126d2
jixuan <420310979@qq.com>
2015-03-06 14:56:28 +0800

    Collections.emptyList()返回的空列是不可改变的

---
+++ src/com/jixuan/tij/reusingclasses/CollectionsEmptyListTest.java
@@ -0,0 +1,22 @@
+package com.jixuan.tij.reusingclasses;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.jixuan.tij.util.Print.println;
+
+/**
+ * @author jixuan
+ *         Create on 15/3/6.
+ */
+public class CollectionsEmptyListTest {
+    public static void main(String[] args) {
+        List<String> testadd = Collections.emptyList();
+        List<String> testadd2 = Collections.EMPTY_LIST;
+        testadd.add("aaaaa");
+//        这两种调用都不行，因为
+//        Returns the empty list (immutable不可改变的). This list is serializable.
+        println(testadd);
+        println(testadd2);
+    }
+}
