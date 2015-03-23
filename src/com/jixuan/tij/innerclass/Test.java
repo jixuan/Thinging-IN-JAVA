bd50bb5d7bd21316973b7731ecc85bd26dc344f5
jixuan <420310979@qq.com>
2015-03-23 16:08:38 +0800

    创建内部类对象

--- src/com/jixuan/tij/innerclass/Test.java
+++ src/com/jixuan/tij/innerclass/Test.java
@@ -8,15 +8,16 @@ import static com.jixuan.tij.util.Print.printnb;
  *         Create on 15/3/23.
  */
 public class Test {
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         Parcel p = new Parcel();
         p.ship("Tasmania");
         Parcel p2 = new Parcel();
-        *//*要在外部类之外的地方，创建内部类的对象，必须具体指明这个对象的类型：OutClassName.InnerClassName*//*
+     //   要在外部类之外的地方，创建内部类的对象，必须具体指明这个对象的类型：OutClassName.InnerClassName
         Parcel.Contents c = p2.contents();
+        Parcel.Contents c2 = p2.new Contents();//用.new 来创建内部类对象
         Parcel.Distination d = p2.to("Borneo");
-    }*/
-   public static void main(String[] args) {
+    }
+ /*  public static void main(String[] args) {
        Sequence sequence = new Sequence(10,"intSeque");
        for (int i = 0; i < 10; i++) {
            sequence.add(Integer.toString(i));
@@ -27,5 +28,5 @@ public class Test {
            selector.next();
        }
        println(selector.toString());
-   }
+   }*/
 }
