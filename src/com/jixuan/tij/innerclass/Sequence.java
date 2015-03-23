36e16773d22c968c3ab0e4a3c3fdf5bd8586b22c
jixuan <420310979@qq.com>
2015-03-23 15:02:45 +0800

    一个队列里包含一个选择器

---
+++ src/com/jixuan/tij/innerclass/Sequence.java
@@ -0,0 +1,53 @@
+package com.jixuan.tij.innerclass;
+
+/**
+ * @author jixuan
+ *         Create on 15/3/23.
+ */
+public class Sequence {
+    private int next = 0;
+    private Object[] items;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length) {
+            items[next++] = x;
+        }
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+
+        @Override
+        public boolean end() {
+            return i == items.length;
+        }
+
+        @Override
+        public Object current() {
+            return items[i];
+        }
+
+        @Override
+        public void next() {
+            if (i < items.length) i++;
+        }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+
+}
+
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
--- src/com/jixuan/tij/innerclass/Test.java
+++ src/com/jixuan/tij/innerclass/Test.java
@@ -1,16 +1,29 @@
 package com.jixuan.tij.innerclass;
 
+import static com.jixuan.tij.util.Print.printnb;
+
 /**
  * @author jixuan
  *         Create on 15/3/23.
  */
 public class Test {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         Parcel p = new Parcel();
         p.ship("Tasmania");
         Parcel p2 = new Parcel();
-        /*要在外部类之外的地方，创建内部类的对象，必须具体指明这个对象的类型：OutClassName.InnerClassName*/
+        *//*要在外部类之外的地方，创建内部类的对象，必须具体指明这个对象的类型：OutClassName.InnerClassName*//*
         Parcel.Contents c = p2.contents();
         Parcel.Distination d = p2.to("Borneo");
-    }
+    }*/
+   public static void main(String[] args) {
+       Sequence sequence = new Sequence(10);
+       for (int i = 0; i < 10; i++) {
+           sequence.add(Integer.toString(i));
+       }
+       Selector selector = sequence.selector();
+       while (!selector.end()) {
+           printnb(selector.current() + " ");
+           selector.next();
+       }
+   }
 }
