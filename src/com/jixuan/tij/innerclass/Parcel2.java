065773e80b6065a83c8933d16c316dc4112269cf
jixuan <420310979@qq.com>
2015-03-31 17:24:44 +0800

    方法内的内部类：方法外无法访问

---
+++ src/com/jixuan/tij/innerclass/Parcel2.java
@@ -0,0 +1,25 @@
+package com.jixuan.tij.innerclass;
+
+/**
+ * @author jixuan
+ *         Create on 15/3/31.
+ */
+public class Parcel2 {
+    /*在方法外，就无法访问PD的类*/
+    public Distination distination(String sss) {
+        class PDistination implements Distination {
+            private String label;
+
+            public PDistination(String label) {
+                this.label = label;
+            }
+
+            @Override
+            public String readLabel() {
+                return label;
+            }
+        }
+        return new PDistination(sss);
+    }
+
+}
