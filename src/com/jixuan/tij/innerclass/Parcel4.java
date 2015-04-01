6191aea2c98b0e084967cc8fbf62047d42f534da
jixuan <420310979@qq.com>
2015-04-01 09:59:29 +0800

    匿名内部类

--- src/com/jixuan/tij/innerclass/Parcel4.java
+++ src/com/jixuan/tij/innerclass/Parcel4.java
@@ -7,9 +7,11 @@ import static com.jixuan.tij.util.Print.println;
  *         Create on 15/3/31.
  */
 public class Parcel4 {
-    public Contents contents(){
+    /*匿名内部类实现接口*/
+    public Contents contents() {
         return new Contents() {
-            private int i=7;
+            private int i = 7;
+
             @Override
             public int value() {
                 return i;
@@ -17,9 +19,21 @@ public class Parcel4 {
         };
     }
 
+    /*匿名内部类重写方法*/
+    public Wapping wapping(int x) {
+        return new Wapping(x) {
+            @Override
+            public int value() {
+                return (super.value() * 47);
+            }
+        };
+    }
+
     public static void main(String[] args) {
         Parcel4 p4 = new Parcel4();
         Contents c = p4.contents();
+        Wapping w = p4.wapping(23);
         println(c.value());
+        println(w.value());
     }
 }
---
+++ src/com/jixuan/tij/innerclass/Wapping.java
@@ -0,0 +1,17 @@
+package com.jixuan.tij.innerclass;
+
+/**
+ * @author jixuan
+ *         Create on 15/4/1.
+ */
+public class Wapping {
+    private int i;
+
+    public Wapping(int i) {
+        this.i = i;
+    }
+
+    public int value() {
+        return i;
+    }
+}
